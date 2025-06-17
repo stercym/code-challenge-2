f81bb1f908f52fd6de5d93112ad5f2eac5020fef
Stercy Mutuku <mutukustercy93@gmail.com>
2025-06-17 20:26:31 +0300

    formating and cleanup console logs

--- src/index.js
+++ src/index.js
@@ -1,49 +1,43 @@
 
 myButton = document.getElementById("submitButton");
 myButton.addEventListener("click", formSubmitHandler);
-let firstNameInput = document.getElementById('fname')
-let lastNameInput = document.getElementById('lname')
+let firstNameInput = document.getElementById('fname');
+let lastNameInput = document.getElementById('lname');
 let guestEmail = document.getElementById('email');
 let rsvpCheckbox = document.getElementById('rsvp');
 let guestCategory = document.getElementById('category');
+
+// add event listeners
+firstNameInput.addEventListener("input", firstNameHandler);
+lastNameInput.addEventListener("input", lastNameHandler);
+email.addEventListener("change", emailHandler);
 
 function buttonClickFn() {
     alert("Button has been clicked");
 }
 
-console.log(firstNameInput)
-firstNameInput.addEventListener("input", firstNameHandler)
-
 function firstNameHandler(e) {
-    guestFirstName = e.target.value
-    console.log(guestFirstName)
+    guestFirstName = e.target.value;
+    console.log(guestFirstName);
 }
-
-
-console.log(lastNameInput)
-lastNameInput.addEventListener("input", lastNameHandler)
 
 function lastNameHandler(e) {
-    console.log(e)
-    console.log(e.target.value)
-    guestLastName = e.target.value
-    console.log(guestLastName)
+    console.log(e);
+    console.log(e.target.value);
+    guestLastName = e.target.value;
+    console.log(guestLastName);
 }
 
-
-console.log(guestEmail)
-email.addEventListener("change", emailHandler)
-
 function emailHandler(e) {
-    console.log(e)
-    console.log(e.target.value)
-    email = e.target.value
-    console.log(email)
+    console.log(e);
+    console.log(e.target.value);
+    email = e.target.value;
+    console.log(email);
 }
 
 function formSubmitHandler(e) {
-    e.preventDefault()
-    guestList = document.getElementById("GuestsList")
+    e.preventDefault();
+    guestList = document.getElementById("GuestsList");
     let numberOfGuests = 0;
 
     let guestRSVP = "Not Attending";
@@ -59,9 +53,9 @@ function formSubmitHandler(e) {
     }
 
     if (numberOfGuests < 10) {
-        console.log({ firstName: firstNameInput, lastName: lastNameInput, email: guestEmail, RSVP: guestRSVP })
+        console.log({ firstName: firstNameInput, lastName: lastNameInput, email: guestEmail, RSVP: guestRSVP });
 
-        newGuest = document.createElement("li")
+        newGuest = document.createElement("li");
         newGuest.textContent = `Name: ${guestFirstName} ${guestLastName} | Email: ${email} | RSVP: ${guestRSVP} | Date Created: ${new Date()}`;
 
         //add color classes
@@ -75,25 +69,25 @@ function formSubmitHandler(e) {
 
         //delete button
         deleteButton = document.createElement("button");
-        deleteButton.textContent = "Remove"
-        deleteButton.addEventListener("click", deleteButtonHandler)
+        deleteButton.textContent = "Remove";
+        deleteButton.addEventListener("click", deleteButtonHandler);
         newGuest.appendChild(deleteButton);
 
-        guestList.appendChild(newGuest)
+        guestList.appendChild(newGuest);
 
         //clear fields
         clearFields();
 
     } else {
-        alert('Guest list limit reached')
+        alert('Guest list limit reached');
     }
 
 }
 
 function deleteButtonHandler(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    //e.target.parentNode.parentNode takes us to the ordered list to delete it
+    //e.target.parentNode is the <li> item to be deleted from the <ol> (e.target.parentNode.parentNode)
     let choice = confirm('Are you sure you want to delete?');
     if (choice) {
         e.target.parentNode.parentNode.removeChild(e.target.parentNode);
